abe2ab44b02ccf4f5e047848fcff281eb01a3cb7
SEC\user <dorothy_007@naver.com>
2019-07-06 03:36:12 +0900

    frontend - axios

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from  'axios';
 
 class App extends Component {
   state = {
@@ -6,12 +7,27 @@ class App extends Component {
     name : '초기이름',
   }
 
+  getCompany =() => {
+  // axios
+  axios.get('http://localhost:4000/company')
+    .then(response=>{
+      console.log(response.data);
+      this.setState({
+        title : response.data.title,
+        name : response.data.name
+      })
+    })
+    .catch(error=>{
+      console.log(error);
+    });
+  }
+
   render(){
     const { title, name } = this.state;
     return(
       <div>
         <div>백엔드 통신해보기</div>
-        <button>통신시작</button>
+        <button onClick={this.getCompany}>통신시작</button>
         <div>{title}</div>
         <div>{name}</div>
       </div>
