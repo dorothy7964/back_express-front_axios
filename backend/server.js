fc3a99ef80a4ef34abb51fc3090553b84739c59f
SEC\user <dorothy_007@naver.com>
2019-07-06 03:37:16 +0900

    backend - cors = 통신 수락하기

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')();   // 통신 수락해주기 : cors
 
+app.use(cors);
 app.listen(4000);
 app.get('/company',function(req,res){
   let data = ({ title : 'company', name : '회사명' });    // 응답을 json으로
